ced2b728e3e2763255dac7e176d0d9ff1528ade7
srg-kartashov <34143453+srg-kartashov@users.noreply.github.com>
2017-12-15 21:02:43 +0200

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="ScheduleDataBaseWebApplication.Global_asax" Language="C#" %>
---
+++ Main.master.designer.cs
@@ -0,0 +1,51 @@
+//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace ScheduleDataBaseWebApplication {
+    
+    
+    public partial class MainMaster {
+        
+        /// <summary>
+        /// ContentSplitter элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxSplitter.ASPxSplitter ContentSplitter;
+        
+        /// <summary>
+        /// ASPxNavBar1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxNavBar.ASPxNavBar ASPxNavBar1;
+        
+        /// <summary>
+        /// MainContent элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// XmlDataSourceLeft элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSourceLeft;
+    }
+}
